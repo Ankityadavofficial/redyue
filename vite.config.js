4f819c985034e18ffe45cd625408a5a02b1c5b1e
Ankit Yadav <ankityadav9992839225@gmail.com>
2024-12-23 19:03:16 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Optional: Adjust the base path if needed for deployment
+  server: {
+    open: true, // Opens the app in the browser on dev server start
+  }
+});
